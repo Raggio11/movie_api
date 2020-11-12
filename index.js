87f7561cd2340f9beb0c178ce8057b6396dba845
Raggio11 <Raggio11@users.noreply.github.com>
2020-11-11 17:14:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require("express"),
-  morgan = require("morgan");
-
+const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser"),
   methodOverride = require("method-override");
 
@@ -16,12 +15,24 @@ app.use(methodOverride());
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Opps and error has occured");
+  res.status(500).send("Something broke!");
 });
-app.get("/documentation", (req, res) => {
+
+let myLogger = (req, res, next) => {
+  console.log(req.url);
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send("This is for all the movies");
+});
+
+app.use(express.static("/documentation"), (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
 });
 
 app.get("/movies", (req, res) => {
-  res.json(movies);
+  res.send("top 10 Movies");
 });
+
+app.listen(8080, () => console.log("Your app is listening on port 8080."));
