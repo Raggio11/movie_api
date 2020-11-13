9ea97299a424c103a71ee28ca05e4781643e0426
Raggio11 <Raggio11@users.noreply.github.com>
2020-11-12 22:26:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ morgan = require("morgan");
 const app = express();
 
 app.use(morgan("common"));
-app.use(bodyParser.json());
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+app.use(express.static("public"));
 
-let myLogger = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
+let topTenMovies = [
+  {
+    title: "Spider-man (2002)",
+    cast: "Tobey Maguire, Kristen Dunst, Willem Dafoe, James Franco "
+  },
+  {
+    title: "Iron Man",
+    cast: "Rober Downey Jr., Jon Favreau, Gwyneth Paltrow"
+  }
+];
 
 app.get("/", (req, res) => {
-  res.send("This is for all the movies");
-});
-
-app.use(express.static("/documentation"), (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
+  res.send("Welcome to myFlix!");
 });
 
 app.get("/movies", (req, res) => {
-  res.json("top 10 Movies");
+  res.json(topTenMovies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(8080, () => console.log("Your app is listening on port 8080."));
