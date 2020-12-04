c6bbb9c8926234d493b819302119526e16aea396
Raggio11 <Raggio11@users.noreply.github.com>
2020-12-04 01:10:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require("express");
-morgan = require("morgan");
+const newLocal = "express";
+const express = require(newLocal);
+const newLocal_1 = "morgan";
+morgan = require(newLocal_1);
+
+bodyParser = require('body-parser'),
 
 mongoose = require("mongoose");
 Models = require("./models.js");
@@ -7,24 +11,29 @@ Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect(process.env.CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://Raggio:ELQyag1JwMItgCF4@myflixdb.clhzr.mongodb.net/myFlixDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(morgan("common"));
 
 app.use(express.static("public"));
 
 //Welcome page-it is working
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Welcome to myFlix!");
 });
 
 //Get all movies - it is working
-app.get("/movies", (req, res) => {
+app.get("/movies", (_req, res) => {
   Movies.find()
     .then(movies => {
       res.status(201).json(movies);
@@ -36,7 +45,7 @@ app.get("/movies", (req, res) => {
 });
 
 //gets all users - it is working
-app.get("/users", (req, res) => {
+app.get("/users", (_req, res) => {
   Users.find()
     .then(users => {
       res.status(201).json(users);
@@ -88,61 +97,112 @@ app.get("/movies/Director/:Name", (req, res) => {
     });
 });
 
-// Gets User by Username - Kinda works. may need future update
-app.get("/users/:Username", (req, res) => {
-  Users.findOne({ Username: req.params.username })
-    .then(users => {
-      res.json(users);
+// Gets User by username - Works perfectly
+app.get("/users/:username", (req, res) => {
+  debugger;
+  Users.findOne({ username: req.params.username })
+    .then(user => {
+      res.json(user);
     })
     .catch(err => {
-      console.error(err);
+      console.error(err);s
       res.status(500).send("Error: " + err);
     });
 });
-// Allows to add new user - can not get it to work
-app.post("/users", (req, res) => {
-  Users.findOne({ Username: req.body.Username })
-    .then(users => {
-      if (users) {
-        return res.status(400).send(req.body.Username + " already exists");
-        console.log("middle section");
+// Allows to add new user - Works perfectly
+app.post('/users', (req, res) => {
+  Users.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.username + 'already exists');
       } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthdate: req.body.Birthdate
-        })
-          .then(users => {
-            res.status(201).json(users);
+        Users
+          .create({
+              username: req.body.username,
+              password: req.body.password,
+              email: req.body.email,
+              Birthday: req.body.Birthday
           })
-          .catch(error => {
-            console.error(error);
-            res.status(500).send("Error: " + error);
-          });
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
-      res.status(500).send("Error: " + error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+//Update user by username - it works!!
+app.put('/users/:username', (req, res) => {
+  Users.findOneAndUpdate({ username: req.params.username }, {
+    $set:
+    {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+//add favorite movie to a specific user - It works
+app.put('/users/:username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ username: req.params.username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+// Deletes a favorite movie from a user - it works
+app.delete('/users/:username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ username: req.params.username }, {
+     $pull: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+// Deletes a user by username - it works
+app.delete('/users/:username', (req, res) => {
+  Users.findOneAndRemove({ username: req.params.username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.username + ' was not found');
+      } else {
+        res.status(200).send(req.params.username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
-app.post("/users/:Username/Movies/:MovieID", (req, res) => {
-  Users.findOneAndUpdate(
-    { Username: req.params.Username },
-    { $push: { FavoriteMovies: req.params._id } }
-  );
-});
-
-app.delete("/users/:Username/Movies/:MovieID", (req, res) => {
-  Users.findOneAndUpdate(
-    { Username: req.params.Username },
-    { $pull: { FavoriteMovies: req.params.MovieID } }
-  );
-});
-
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
