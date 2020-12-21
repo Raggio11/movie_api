8fd069ba84743a7ae880cf7e7cf68907f79618c7
Raggio11 <Raggio11@users.noreply.github.com>
2020-12-20 18:44:30 -0700

    Update models.js

--- models.js
+++ models.js
@@ -23,7 +23,7 @@ let userSchema = mongoose.Schema({
   username: { type: String, required: true},
   password: { type: String, required: true},
   email: { type: String, required: true},
-  Birthday: Date,
+  Birthdate: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
 });
 
